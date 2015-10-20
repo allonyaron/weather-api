6a4642536ee63a56bdc6a42c5680232811b350bc
Allon Yaron <allonyaron@gmail.com>
2015-10-20 16:10:12 -0400

    check request error

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ app.get('/api/weather/:location', function(req,res) {
 	//use open weather map api with city name
 	var uri = "http://api.openweathermap.org/data/2.5/weather?q=" + req.params.location + ",us&units=imperial&appid=7a7876c8bc664c64008cd5bbbb4440c6";
 	request(uri, function(error, response, body) {
-		var parsed = JSON.parse(body);
-  		res.json(parsed);
+		if(error) {
+			console.log(error);
+		} else {
+			var parsed = JSON.parse(body);
+	  		res.json(parsed);	
+		}
 	});
 
 });
