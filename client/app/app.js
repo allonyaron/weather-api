65305fd4209d38fa8ba5135af9871483b99b9f6f
Allon Yaron <allonyaron@gmail.com>
2015-10-20 19:59:30 -0400

    added weather display widget

--- client/app/app.js
+++ client/app/app.js
@@ -1,10 +1,12 @@
 var app = angular.module("weatherApp",[]);
 
 app.controller("weatherCtrl", function($scope, $http) {
+	$scope.showWeatherValue = false;
 	$scope.submitLoc = function() {
 		$http.get('/api/weather/' + $scope.location)
 		.success(function(data) {
 			$scope.weather = data;
+			$scope.showWeatherValue = true;
 		})
 		.error(function(data, status, headers, config) {
 	      console.log(status);
