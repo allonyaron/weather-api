ba514f7099d2621cb6f14d9ac7c1206ced2e8849
Allon Yaron <allonyaron@gmail.com>
2015-10-20 11:15:00 -0400

    udpated to use city - return res.json

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ var request = require('request');
 var port = process.env.PORT || 3000;
 
 app.get('/api/weather/:location', function(req,res) {
-
-	var uri = "http://api.openweathermap.org/data/2.5/weather?zip=" + req.params.location + ",us&units=imperial&appid=7a7876c8bc664c64008cd5bbbb4440c6";
+	//use open weather map api with city name
+	var uri = "http://api.openweathermap.org/data/2.5/weather?q=" + req.params.location + ",us&units=imperial&appid=7a7876c8bc664c64008cd5bbbb4440c6";
 	request(uri, function(error, response, body) {
 		var parsed = JSON.parse(body);
-  		res.send(parsed);
+  		res.json(parsed);
 	});
 
 });
